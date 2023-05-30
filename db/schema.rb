256da13b810b54484f1a157f7f896f5a054a6e28
Romain DELIMAL <rom.delimal@gmail.com>
2023-05-30 12:07:59 +0200

    fixedRoleUser

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_095327) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
