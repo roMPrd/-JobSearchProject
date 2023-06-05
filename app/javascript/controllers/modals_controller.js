7a9fda79e90101c304b946595eb5ebd3910c366c
Romain DELIMAL <rom.delimal@gmail.com>
2023-06-05 18:17:01 +0200

    logic stimulus hide container on click from add, update and select offer

--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -4,18 +4,49 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
+  toggleContainers() {
+    // get the right container
+    const rightContainer = document.getElementById("offer-info-box");
+    // hide right container
+    rightContainer.classList.add("hidden-container");
 
-  closeModal(event) {
+    //get container modal and add class
+    document.getElementById("modal-add-edit").classList.remove("hidden-container");
+
+    // toggle hidden class for right container or empty modal
+    // if (rightContainer.classList.contains("hidden-container")) {
+    //   rightContainer.classList.remove("hidden-container");
+    // } else { rightContainer.classList.add("hidden-container");
+    //   const modal = document.getElementById("modal");
+    //   modal.innerHTML = "";
+    //   modal.removeAttribute("complete");
+    //   modal.removeAttribute("src");
+    // }
+  }
+
+  closeModal() {
     // prevent refresh
-    // event.preventDefault();
-    // remove modal
-    const modal = document.querySelector(".modal");
+    event.preventDefault();
+
+    // get the left container
+    const rightContainer = document.getElementById("offer-info-box");
+    // toggle hidden class for right container
+    rightContainer.classList.remove("hidden-container");
+
+    // get modal
+    const modal = document.getElementById("modal");
+    // empty modal
     modal.innerHTML = "";
+    //get container modal and remove class
+    document.getElementById("modal-add-edit").classList.add("hidden-container");
+    // modal.classList.remove("index-offer-container-right");
+
+
 
     // remove the src attribute from the modal
-    // modal.removeAttribute("src");
+    modal.removeAttribute("src");
 
     // remove complete attribute from the modal
-    // modal.removeAttribute("complete");
+    modal.removeAttribute("complete");
   }
 }
--- app/javascript/controllers/offer_zoom_controller.js
+++ app/javascript/controllers/offer_zoom_controller.js
@@ -10,15 +10,28 @@ export default class extends Controller {
   }
 
   zoom() {
-    console.log(this.element)
-    console.log("hello from zoom controller")
-    console.log(this.selectOfferTargets)
+    // console.log(this.element)
+    // console.log("hello from zoom controller")
+    // console.log(this.selectOfferTargets)
+
+    // get the right container
+    const rightContainer = document.getElementById("offer-info-box");
+    //get the modal container
+    const modalContainer = document.getElementById("modal-add-edit")
+    // toggle hidden class for right container or empty modal
+    if (rightContainer.classList.contains("hidden-container")) {
+      rightContainer.classList.remove("hidden-container");
+      modalContainer.classList.add("hidden-container");
+    }
+
     //remove move class from all offers
-    document.querySelectorAll(".offer-card-item").forEach((offer) => {
-      offer.classList.remove("red");
+    document.querySelectorAll(".offer-card-item-background").forEach((offer) => {
+      offer.classList.remove("background-gradient");
+      offer.classList.add("background-white");
     })
     //add move class to selected offer
-    this.element.classList.add("red");
+    this.element.classList.remove("background-white");
+    this.element.classList.add("background-gradient");
     //empty offer box
     this.offerBox.innerHTML = " ";
     //render selected offer in offer box
@@ -29,7 +42,7 @@ export default class extends Controller {
   }
 
   moveSelectedOffer() {
-
+    // TODO: or not xD
   }
 
 
